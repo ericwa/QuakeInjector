a4befd5c991dffc7c2187254b50a76ae817e2e6f
Eric Wasylishen <ewasylishen@gmail.com>
2012-01-31 23:55:56 -0700

    fix build

--- src/de/haukerehfeld/quakeinjector/AppBundle.java
+++ src/de/haukerehfeld/quakeinjector/AppBundle.java
@@ -1,16 +1,25 @@
-import com.dd.plist.PropertyListParser;
+import com.dd.plist.*;
+import java.io.File;
 
 public class AppBundle {
-	AppBundle(File bundle) {
-		File infoPlistFile = File(File(bundle, "Contents"), "Info.plist")
+	public static File bundleExecutable(File bundle) {
+		try {
+			File infoPlistFile = new File(new File(bundle, "Contents"), "Info.plist");
+			
+			NSDictionary infoPlist = (NSDictionary)PropertyListParser.parse(infoPlistFile);
+			
+			String bundleExecutable = infoPlist.objectForKey("CFBundleExecutable").toString();
+			
+			File executableFile = new File(new File(new File(bundle, "Contents"), "MacOS"), bundleExecutable);
 		
-		NSDictionary infoPlist = (NSDictionary)PropertyListParser.parser(infoPlistFile);
-		
-		String bundleExecutable = infoPlist.objectForKey("CFBundleExecutable").toString();
-		
-		File executableFile = File(File(File(bundle, "Contents"), "MacOS"), bundleExecutable);
-	
-		System.out.println("Got bundle executable: ");
-		System.out.println(executableFile.toString());
+			System.out.println("Got bundle executable: ");
+			System.out.println(executableFile.toString());
+			
+			return executableFile;
+			
+		} catch (Exception e) {
+			System.out.println("Failed to get bundle executable.");
+			return null;
+		}
 	}
 }
